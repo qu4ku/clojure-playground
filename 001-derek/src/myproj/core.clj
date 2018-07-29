cb2bafc5790094905e1e148b77eed73f6d1ef8e0
qu4ku <qu4ku@hotmail.com>
2018-07-30 00:57:51 +0200

    braveclojure

--- 001-derek/src/myproj/core.clj
+++ 001-derek/src/myproj/core.clj
@@ -1,4 +1,6 @@
 (ns myproj.core
+  (:require [clojure.string :as str])
+  (:require [proto-repl])
   (:gen-class))
 
 ; (defn -main
@@ -15,14 +17,21 @@
 ; (float? 15)
 ; (zero? 0)
 
+; (defn -main
+;   [& args]
+;   (def aLong 15)
+;   (def someVariable "h")
+;   (def aDoubleSec 1.234)
+;   (format "This is a string %s" someVariable)
+;   (format "5 spaces and %5d" aLong)
+;   (format "Leading zeros %04d" aLong)
+;   (format "%-4d left justify" aLong)
+;   (format "3 decimals %.3f" aDoubleSec))
+
+
 (defn -main
   [& args]
-  (def secondString "Hello")
-  (def aLong 15)
-  (def second "h")
-  (def aDouble 1.234)
-  ; (format "This is a string %s" secondString)
-  (format "5 spaces and %5d" aLong)
-  (format "Leading zeros %04d" aLong)
-  (format "%-4d left justify" aLong)
-  (format "3 decimals %.3f" aDouble))
+  (def str1 "This is my 2nd string")
+  (def str2 "")
+  (str/blank? str1))
+  ; (str/blank? str2))
---
+++ braveclojure/clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,9 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (do
+    (println "no prompt here")
+    (+ 1 2)))
