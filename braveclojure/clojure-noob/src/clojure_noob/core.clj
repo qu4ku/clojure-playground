556dc12d7afb3b15a969938202e09196964bbd7f
qu4ku <qu4ku@hotmail.com>
2018-08-02 22:39:45 +0200

    braveclojure

--- braveclojure/clojure-noob/src/clojure_noob/core.clj
+++ braveclojure/clojure-noob/src/clojure_noob/core.clj
@@ -258,3 +258,91 @@
   #(+ % inc-by))
 (def inc3 (inc-maker 3))
 (inc3 7)
+
+
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouht" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+; tests
+; (println asym-hobbit-body-parts)
+(defn print-part
+  [{:keys [name size]}]
+  (println (clojure.string/join ", " [name size])))
+; (print-part {:name "test-head" :size 10})
+
+(for [part asym-hobbit-body-parts]
+  ; (println part))
+  ; (println part)
+  (print-part [part]))
+
+; real functions
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that havea a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+          (into final-body-parts
+                (set [part (matching-part part)])))))))
+(symmetrize-body-parts asym-hobbit-body-parts)
+
+
+(let [x 3]
+  (inc x))
+
+(def dalmatian-list
+  ["Pongo" "Perdita" "Puppy 1" "Puppy 2"])
+(let [dalmatians (take 2 dalmatian-list)]
+  dalmatians)
+
+(def x 0)
+(let [x 1] x)
+
+(def x 0)
+(let [x (inc x)] x)
+
+(let [[pongo & dalmatians] dalmatian-list]
+  [pongo dalmatians])
+
+(into [] (set [:a :a]))
+
+(loop [iteration 0]
+  (println (str "Iteration " iteration))
+  (if (> iteration 4)
+    (println "Goodbye!")
+    (recur (inc iteration))))
+
+(def li ["first" "second" "third"])
+(loop [index 0]
+  (if (get li index)
+    (println (str (get li index)))
+    (recur (inc index))))
+
+()
+(get li 1)
