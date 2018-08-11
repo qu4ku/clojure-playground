3b7e0499467d03de5f470707514d449d9c8cf242
qu4ku <qu4ku@hotmail.com>
2018-08-11 20:49:54 +0200

    braveclojure

--- braveclojure/core-functions/src/core_functions/core.clj
+++ braveclojure/core-functions/src/core_functions/core.clj
@@ -56,3 +56,34 @@
                  {:alias "spider-man" :real "peter parker"}
                  {:alias "santa" :real "your mum"}])
 (map :real identities)
+
+
+(reduce (fn [new-map [key val]]
+          (assoc new-map key (inc val)))
+        {}
+        {:max 30 :min 10})
+
+(assoc (assoc {} :max (inc 30))
+       :min (inc 10))
+
+(take 3 [1])
+(take 3 [1 2 3 4 54])
+
+
+(drop 3 [1 2 3 4 5 6])
+
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(map :day food-journal)
+
+(take-while #(< (:month %) 3) food-journal)
+(drop-while #(< (:month %) 3) food-journal)
