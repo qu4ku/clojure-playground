1f0e44e30c4eee6c6aee6e77e22e7e2325fa3bab
qu4ku <qu4ku@hotmail.com>
2018-08-23 23:07:57 +0200

    braveclojure

--- braveclojure/functional-programming/src/functional_programming/core.clj
+++ braveclojure/functional-programming/src/functional_programming/core.clj
@@ -1,7 +1,51 @@
 (ns functional-programming.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(+ 1 2)
+
+(defn wisdom
+  [words]
+  (str words ", Daniel-san"))
+
+(wisdom "Always bathe on Fridays")
+
+(defn year-end-evaluation
+  []
+  (if (> (rand) 0.5)
+    "You get a raise!"
+    "Better luck next year!"))
+
+(year-end-evaluation)
+
+(defn analyze-file
+  [filename]
+  (analysis (slurp filename)))
+
+(defn analysis
+  [text]
+  (str "Character count: " (count text)))
+
+(def great-baby-name "Rosmary")
+great-baby-name
+
+(let [great-baby-name "Sunny"]
+  great-baby-name)
+great-baby-name
+
+
+(defn sum
+  ([vals] (sum vals 0))
+  ([vals accumulating-total]
+   (if (empty? vals)
+     accumulating-total
+     (sum (rest vals) (+ (first vals) accumulating-total))
+
+
+     (defn sum
+       ([vals] (sum vals 0))
+       ([vals accumulating-total]
+        (if (empty? vals)
+          accumulating-total
+          (recur (rest vals) (+ (first vals) accumulating-total))))))))
+
+(sum [10 20])
