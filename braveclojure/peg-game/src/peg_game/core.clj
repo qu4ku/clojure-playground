27aa404165509d4bf617fa824acc52528f9135fe
qu4ku <qu4ku@hotmail.com>
2018-08-28 19:00:45 +0200

    braveclojure

---
+++ braveclojure/peg-game/src/peg_game/core.clj
@@ -0,0 +1,57 @@
+(ns peg-game.core
+  (require [clojure.set :as set])
+  (:gen-class))
+
+(declare successful-move prompt-move game-over query-rows)
+
+
+(defn tri*
+  "Generate lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+   (let [new-sum (+ sum n)]
+     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(take 10 (tri*))
+
+(def tri (tri*))
+
+(take 5 tri)
+
+(defn triangular?
+  "Is the number triangular? e.g. 1, 3, 6, 10, 15, etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(triangular? 5)
+
+(triangular? 6)
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(row-tri 1)
+(row-tri 2)
+(row-tri 4)
+
+(defn row-num
+  "Returns row number the position belongs to: pos 1 in row 1,
+positions 2 and 3 in row 2, etc"
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
+(row-num 1)
+(row-num 5)
+
+(defn connect
+  "Form a mutual connection between two positions"
+  [board max-pos pos neighbor destination]
+  (if (<= destination max-pos)
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] neighbor))
+            board[[pos destination] [destination pos]])
+    board))
+
+(connect {} 15 1 2 4)
