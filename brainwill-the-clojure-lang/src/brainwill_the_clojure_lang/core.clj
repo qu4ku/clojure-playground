7174ad1576e13b7773682026e7afc86fa8a23866
qu4ku <qu4ku@hotmail.com>
2018-09-10 14:32:41 +0200

    brainwill

--- brainwill-the-clojure-lang/src/brainwill_the_clojure_lang/core.clj
+++ brainwill-the-clojure-lang/src/brainwill_the_clojure_lang/core.clj
@@ -449,3 +449,42 @@
 
 ; ^keyword is shorthand for ^{keyword true}
 ^:foo  ; ^{:foo true}
+
+
+; rock-paper-scissors
+; returns "r" for rock, "p" for paper, "s" for scissors
+; returns nil on invalid input
+
+(defn get-guess []
+  ; we must use println instead of print to immediately flush the buffer, apparently
+  (println "Play your hand: (r)ock, (p)aper, (s)cissors")
+  (let [guess (read-line)]
+    (if (get {"r" true "p" true "s" true} guess) guess)))
+
+; returns 1 for player 1 wins, 2 for player 2 wins, and 0 for a tie
+(defn winner [guess1 guess2]
+  (let [guesses [guess1 guess2]]
+    (cond
+      (= guess1 guess2) 0
+      (= guesses ["p" "r"]) 1
+      (= guesses ["r" "p"]) 2
+      (= guesses ["r" "s"]) 1
+      (= guesses ["s" "r"]) 2
+      (= guesses ["s" "p"]) 1
+      (= guesses ["p" "s"]) 2)))
+
+(defn play-hand []
+  (let [computer-guess (rand-nth ["r" "p" "s"])
+        play-guess (get-guess)
+        winner (winner computer-guess player-guess)]
+    (println "the computer guessed:" computer-guess)
+    (println "You guessed:" player-guess)
+    (cond
+      (= player-guess nill) (println "Your entry was invalid.")
+      (= winner 0) (println "Game tied!")
+      (= winner 1) (println "Computer wins!")
+      (= winner 2) (println "Player wins!"))))
+
+(loop []
+  (play-hand)
+  (recure))
