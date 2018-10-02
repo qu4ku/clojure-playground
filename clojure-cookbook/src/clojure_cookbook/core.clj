b270954d5829e3dabf33c7f612aa1c35c0ec34ed
qu4ku <qu4ku@hotmail.com>
2018-10-03 00:43:31 +0200

    cookbook

--- clojure-cookbook/src/clojure_cookbook/core.clj
+++ clojure-cookbook/src/clojure_cookbook/core.clj
@@ -54,3 +54,77 @@
           s))
 (yelling? "LOUD NOISES!")
 (yelling? "Take a DEEP breath.")
+
+
+; 1.5 converting between characters and integers
+
+; character into its integer value
+(int \a)
+(map int "Hello World")
+; integer into character
+(char 97)
+(map char (range 0x0410 0x042F))
+
+
+; 1.6 formatting string
+(def me {:first-name "kamil" :favarite-language "Clojure"})
+(str "My name is " (:first-name me)
+     ", and I really like to program in " (:favarite-language me))
+(apply str (interpose " " [1 2.00 (/ 3 1) (/ 4 9)]))
+(interpose " " [1 2.00 (/ 3 1) (/ 4 9)])
+
+(defn filename [name i]
+  (format "%03d-%s" i name))
+(filename "my-awesome-file.txt" 42)
+
+; create table using justificatoin
+(defn tableify [row]
+  (apply format "%-20s | %-20s | %-20s" row))
+(def header ["First Name" "Last Name" "Employee ID"])
+(def employees [["Ryan" "Neufeld", 2]
+                ["Luke" "Vanderhart" 1]])
+(->> (concat [header] employees)
+     (map tableify)
+     (mapv println))
+
+; 1.7 searching a string by pattern
+
+; any contiguous groups of numbers
+(re-find #"\d+" "I've just finished reading Fahrenheit 451")
+(re-find #"Bees" "Beads aren't cheap.")
+
+; re-matches matches only if whole string matches
+(re-find #"\w+" "my-params")
+(re-matches #"\w+" "my-params")
+(re-matches #"\w+" "justLetters")
+
+; extract pattern into sequence
+(re-seq #"\w+" "My Favorite Things")
+(re-seq #"\d{3}-\d{4}" "My phone number is 555-1234.")
+
+; extract all of the twitter usernames and hashtags in a tweet
+(defn mentions [tweet]
+  (re-seq #"(@|#)(\w+)" tweet))
+(mentions "so long, @earth, and thangs for all the #fish. #goodbye")
+
+; 1.9 performin find and replace on strings
+(def about-me "My favorite color is green!")
+(clojure.string/replace about-me "green" "red")
+
+(defn de-canadianize [s]
+  (clojure.string/replace s "ou" "o"))
+(de-canadianize "Those Canadian neighbours have coloured behavioiur")
+
+(defn linkify-comment
+  "Add Markdown-style links for any GitHub issue numbers present in comment"
+  [repo comment]
+  (clojure.string/replace comment
+                         #"(\d+)"
+                         (str "[#$1](https://github.com/" repo "/issues/$1)")))
+(linkify-comment "next/big-thing" "As soon as we fix #42 and #1337 we should be set to release!")
+
+; linkify-comment rewritten with linkification as a separate function
+(defn linkify [repo [full-match id]]
+  (str "[" full-match "](https://github.com/" repo "/issues/" id ")"))
+(defn linkify-comment [repo comment]
+  (clojure.string/replace comment #"#(\d+)" (partial linkify repo)))
