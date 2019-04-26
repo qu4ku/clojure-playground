a8283ec1fd46de8c1c743ee02d2cbcf6228482a9
qu4ku <qu4ku@hotmail.com>
2019-04-26 16:08:28 +0200

    tictactoe

--- tictactoe/src/tictactoe/core.cljs
+++ tictactoe/src/tictactoe/core.cljs
@@ -1,6 +1,6 @@
-(ns tictactoe.core
-    (:require [reagent.core :as reagent :refer [atom]]))
-
+(ns ^figwheel-always tictactoe.core
+    (:require [reagent.core :as reagent :refer [atom]]
+              [cljs.test :refer-macros [deftest is]]))
 (enable-console-print!)
 
 (defn new-board [n]
@@ -8,91 +8,130 @@
 
 (def board-size 3)
 
+(def win-length 3)
+
 (defonce app-state 
   (atom {:text "Welcome to tic tac toe"
-         :board (new-board board-size)}))
+         :board (new-board board-size)
+         :game-status :in-progress}))
 
-; (defn compute-move [board])
-  ; (let [board (:board @app-state)
-  ;       remaining-spotes [(for [i (range board-size)
-  ;                               j (range board-size)
-  ;                               :when (= (get-in board [j i]) "B")]
-  ;                           [i j])]
-  ;       move (rand-nth remaining-spotes)]
-  ; 
-  ;   ; (println (assoc-in board move "C"))
-  ;   (println remaining-spotes)))
-  ;   ; (assoc-in board move "C")))
-(defn compute-move [board]
-  (let [board (:board @app-state)
-        remaining-spotes (for [i (range board-size)
-                                j (range board-size)
+(defn computer-move [board]
+  (let [remaining-spots (for [i (range board-size)
+                               j (range board-size)
                                :when (= (get-in board [j i]) "B")]
-                           [i j])
-        move (rand-nth remaining-spotes)]
-    (assoc-in board move "C")))
+                          [i j])
+        move (when (seq remaining-spots)
+               (rand-nth remaining-spots))]
+    (if move
+      (assoc-in board move "C")
+      board)))
 
-; (deftest computer-move-test
-;   (is (= [["C"]]
-;          (computer-move [["B"]]))))
+(deftest computer-move-test
+  (is (= [["C"]]
+         (computer-move [["B"]])))
+  (is (= [["P"]]
+         (computer-move [["P"]]))))
+
+(defn straight [owner board [x y] [dx dy] n]
+  (every? true?
+          (for [i (range n)]
+            (= (get-in board [(+ (* dx i) x)
+                              (+ (* dy i) y)])
+               owner))))
+
+(defn win? [owner board n]
+  (some true?
+        (for [i (range board-size)
+              j (range board-size)
+              dir [[1 0] [0 1] [1 1] [1 -1]]]
+          (straight owner board [i j] dir n))))
+
+(deftest win?-test
+  (is (win? "P" [["P"]] 1))
+  (is (not (win? "P" [["P"]] 2)))
+  (is (win? "P" [["C" "P"]
+                 ["P" "C"]] 2)))
+
+(defn full? [board]
+  (every? #{"P" "C"} (apply concat board)))
+
+(defn game-status [board]
+  (cond
+    (win? "P" board win-length) :player-victory
+    (win? "C" board win-length) :computer-victory
+    (full? board) :draw
+    :else :in-progress))
+
+(defn update-status [state]
+  (assoc state :game-status (game-status (:board state))))
+
+(defn check-game-status [state]
+  (-> state
+      (update-in [:board] computer-move)
+      (update-status)))
 
 (defn blank [i j]
-  [:rect {:width 0.9
-          :height 0.9
-          :fill "grey"
-          :x i
-          :y j
-          :on-click
-          (fn rect-click [e]
-            (prn "You clicked me!" i j)
-            (swap! app-state assoc-in [:board j i] "P")
-            (swap! app-state assoc :board compute-move)
-            (prn (:board app-state)))}])
+  [:rect 
+   {:width 0.9
+    :height 0.9
+    :fill "grey"
+    :x (+ 0.05 i)
+    :y (+ 0.05 j)
+    :on-click
+    (fn blank-click [e]
+      (when (= (:game-status @app-state) :in-progress)
+        (swap! app-state assoc-in [:board j i] "P")
+        (if (win? "P" (:board @app-state) win-length)
+          (swap! app-state assoc :game-status :player-victory)
+          (swap! app-state check-game-status))))}])
 
 (defn circle [i j]
   [:circle
-   {:r 0.45
-    :cx (+ 0.45 i)
-    :cy (+ 0.45 j)
-    :fill "red"}])
+   {:r 0.35
+    :stroke "green"
+    :stroke-width 0.12
+    :fill "none"
+    :cx (+ 0.5 i)
+    :cy (+ 0.5 j)}])
 
 (defn cross [i j]
-  [:g {:stroke "black"
-       :stroke-width 0.2
+  [:g {:stroke "darkred"
+       :stroke-width 0.4
        :stroke-linecap "round"
-       :transform (str "translate(" (+ 0.20 i) "," (+ 0.25 j) ") " "scale(0.5)")}
-   [:line {:x1 0 :y1 0 :x2 1 :y2 1}]
-   [:line {:x1 1 :y1 0 :x2 0 :y2 1}]])
+       :transform (str "translate(" (+ 0.5 i) "," (+ 0.5 j) ") " "scale(0.3)")}
+   [:line {:x1 -1 :y1 -1 :x2 1 :y2 1}]
+   [:line {:x1 1 :y1 -1 :x2 -1 :y2 1}]])
 
 (defn tictactoe []
   [:center
    [:h1 (:text @app-state)]
-   (into
-     [:svg 
-      {:view-box (str "0 0 " board-size " " board-size)
-       :width 500
-       :height 500}
-      (for [i (range board-size)
-            j (range board-size)]
-        (case (get-in @app-state [:board j i])
-          "B" [blank i j]
-          "C" [circle i j]
-          "P" [cross i j]))])
-   [:p
-    [:button 
+   [:h2
+    (case (:game-status @app-state)
+      :player-victory "you won!"
+      :computer-victory "computer wins."
+      :draw "draw."
+      "")
+    [:button
      {:on-click
       (fn new-game-click [e]
-        (swap! app-state assoc :board (new-board board-size)))}
-     "New Game"]]])
-
+        (swap! app-state assoc
+               :board (new-board board-size)
+               :game-status :in-progress))}
+     "New Game"]]
+   (into
+    [:svg
+     {:view-box (str "0 0 " board-size " " board-size)
+      :width 500
+      :height 500}]
+    (for [i (range board-size)
+          j (range board-size)]
+      (case (get-in @app-state [:board j i])
+        "B" [blank i j]
+        "P" [circle i j]
+        "C" [cross i j])))])
                   
-
 (reagent/render-component [tictactoe]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-  ; (swap! app-state assoc-in [:board 0 0] 2))
   (prn (:board @app-state)))
