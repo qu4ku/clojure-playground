be95409dd97bc607a592a9b06f978ea6fc8cabce
qu4ku <qu4ku@hotmail.com>
2019-11-19 00:30:14 +0100

    hackerrank

---
+++ hackerrank-fp/src/hackerrank_fp/009-pentagonal-numbers.clj
@@ -0,0 +1,23 @@
+(ns hackerrank-fp.009-pentagonal-numbers)
+; https://www.hackerrank.com/challenges/pentagonal-numbers
+
+(def n (Integer/parseInt (read-line)))
+
+(defn pentagonal [n]
+  (/ 
+   (* n (- (* 3 n) 1)) 
+   2))
+
+(doseq [_ (range n)]
+    (println (pentagonal (Integer/parseInt (read-line)))))
+
+
+; compact
+(dotimes [t (read)]
+  (println "test"))
+
+; solutions knowledge
+(dotimes [t 4]
+  (println "test"))
+
+(Integer. "4")
