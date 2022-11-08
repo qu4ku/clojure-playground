b14f1659a6c4b5fd4d6921b908abbf404d71b154
qu4ku <qu4ku@hotmail.com>
2022-11-08 20:09:47 +0100

    animal form, working

--- 019-form-clojurescript/src/animal_form/subs.cljs
+++ 019-form-clojurescript/src/animal_form/subs.cljs
@@ -6,3 +6,18 @@
  ::name
  (fn [db]
    (:name db)))
+
+(re-frame/reg-sub
+ ::form
+ (fn [db [_ id]]
+   (get-in db [:form id] "")))
+
+(re-frame/reg-sub
+ ::form-is-valid?
+ (fn [db [_ form-ids]]
+   (every? #(get-in db [:form %]) form-ids)))
+
+(re-frame/reg-sub
+ ::animals
+ (fn [db]
+   (get db :animals [])))
--- 019-form-clojurescript/src/animal_form/views.cljs
+++ 019-form-clojurescript/src/animal_form/views.cljs
@@ -1,12 +1,43 @@
 (ns animal-form.views
   (:require
    [re-frame.core :as re-frame]
-   [animal-form.subs :as subs]
-   ))
+   [animal-form.events :as events]
+   [animal-form.subs :as subs]))
+
+(def animal-types ["Dog" "Cat" "Mouse"])
+
+(defn animal-list []
+  (let [animals @(re-frame/subscribe [::subs/animals])]
+    [:div
+    [:h1 "animal list"]
+    [:ul
+      (map (fn [{:keys [animal-type animal-name]}]
+            [:li {:key animal-name} (str animal-name " (" animal-type ")")]) animals)]]))
+
+(defn text-input [id label]
+  (let [value (re-frame/subscribe [::subs/form id])]
+   [:div.field
+    [:label.label label]
+    [:div.control
+     [:input.input {:value @value
+                    :on-change #(re-frame/dispatch [::events/update-form id (-> % .-target .-value)])
+                    :type "text" :placeholder "Text input"}]]]))
+
+(defn select-input [id label options]
+  (let [value (re-frame/subscribe [::subs/form id])]
+    [:div.field
+      [:label.label label]
+      [:div.control
+        [:div.select
+         [:select {:value @value :on-change #(re-frame/dispatch [::events/update-form id (-> % .-target .-value)])}
+            [:option {:value ""} "Please select"]
+            (map (fn [o] [:option {:key o :value o} o]) options)
+            ]]]]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1
-      "Hello from " @name]
-     ]))
+    [:div.section
+     [animal-list]
+     [text-input :animal-name "Animal name"]
+     [select-input :animal-type "Animal Type" animal-types]
+     [:button.button.is-primary {:on-click #(re-frame/dispatch [::events/save-form])}"Save"]]))
