f6e6a50e4874a16bde89df9e7ccdeab97873b08e
qu4ku <qu4ku@hotmail.com>
2022-12-02 23:23:20 +0100

    reddit viewer working

--- 021-reddit-viewer/src/reddit_viewer/core.cljs
+++ 021-reddit-viewer/src/reddit_viewer/core.cljs
@@ -1,11 +1,8 @@
 (ns reddit-viewer.core
-    (:require
-      [ajax.core :as ajax]
-      [reagent.core :as r]
-      [reagent.dom :as d]))
-
-;; -------------------------
-;; Views
+  (:require
+    [ajax.core :as ajax]
+    [reagent.core :as r]
+    [reddit-viewer.chart :as chart]))
 
 (defonce posts (r/atom nil))
 
@@ -14,26 +11,74 @@
 
 (defn load-posts []
   (ajax/GET "http://www.reddit.com/r/Catloaf.json?sort=new&limit=10"
-            {:handler #(->> (get-in % [:data :children])
-                            (map :data)
-                            (find-posts-with-preview)
-                            (reset! posts))
+            {:handler         #(->> (get-in % [:data :children])
+                                    (map :data)
+                                    (find-posts-with-preview)
+                                    (reset! posts))
              :response-format :json
-             :keywords? true}))
+             :keywords?       true}))
 
-(defn display-post [{:keys [url]}]
-  (when url [:img {:src url}]))
+
+;; -------------------------
+;; Views
+
+(defn display-post [{:keys [permalink subreddit title score url]}]
+  [:div.card.m-2
+   [:div.card-block
+    [:h4.card-title
+     [:a {:href (str "http://reddit.com" permalink)} title " "]]
+    [:div [:span.badge.badge-info {:color "info"} subreddit " score " score]]
+    [:img {:width "300px" :src url}]]])
+
+(defn display-posts [posts]
+  (when-not (empty? posts)
+    [:div
+     (for [posts-row (partition-all 3 posts)]
+       ^{:key posts-row}
+       [:div.row
+        (for [post posts-row]
+          ^{:key post}
+          [:div.col-4 [display-post post]])])]))
+
+(defn sort-posts [title sort-key]
+  (when-not (empty? @posts)
+    [:button.btn.btn-secondary
+     {:on-click #(swap! posts (partial sort-by sort-key))}
+     (str "sort posts by " title)]))
+
+(defn navitem [title view id]
+  [:li.nav-item
+   {:class-name (when (= id @view) "active")}
+   [:a.nav-link
+    {:href     "#"
+     :on-click #(reset! view id)}
+    title]])
+
+(defn navbar [view]
+  [:nav.navbar.navbar-toggleable-md.navbar-light.bg-faded
+   [:ul.navbar-nav.mr-auto.nav
+    {:className "navbar-nav mr-auto"}
+    [navitem "Posts" view :posts]
+    [navitem "Chart" view :chart]]])
 
 (defn home-page []
-  [:div
-   [:h2 "Welcome to Reagent test"]
-   [display-post (first @posts)]])
+  (r/with-let [view (r/atom :posts)]
+    [:div
+     [navbar view]
+     [:div.card>div.card-block
+      [:div.btn-group
+       [sort-posts "score" :score]
+       [sort-posts "comments" :num_comments]]
+      (case @view
+        :chart [chart/chart-posts-by-votes posts]
+        :posts [display-posts @posts])]]))
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (d/render [home-page] (.getElementById js/document "app")))
+  (r/render [home-page] (.getElementById js/document "app")))
 
 (defn init! []
-  (mount-root))
+  (mount-root)
+  (load-posts))
